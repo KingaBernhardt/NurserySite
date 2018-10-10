bc6baa12a9776e3288468de48a845c740d6107dc
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-10-10 14:30:53 +0100

    add(NurseryApp):Nurserycontext to create database

---
+++ WebApplicationNursery/Repositories/NurseryContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplicationNursery.Models;
+
+namespace WebApplicationNursery.Repositories
+{
+    public class NurseryContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Group> Groups { get; set; }
+
+        public NurseryContext(DbContextOptions<NurseryContext> options) : base(options)
+        {
+
+        }
+    }
+}
