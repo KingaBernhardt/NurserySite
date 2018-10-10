311b49de460acced8d310775dbc33c3d98946d2f
Kinga Bernhardt <kinga.bernhardt@gmail.com>
2018-10-10 13:17:26 +0100

    feat(NurseryApp):user,group model and controller

---
+++ WebApplicationNursery/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebApplicationNursery.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebApplicationNursery/Models/GroupModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationNursery.Models
+{
+    public class GroupModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int NumbersOfChildren { get; set; }
+        public string Teacher { get; set; }
+        public string TeacherAssistant { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
